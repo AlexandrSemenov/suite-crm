958937e3c7edf179d86633730d3b6ba5112ebc83
Alexander Semenov <dfmdeveloper2@sedoc.it>
2017-04-24 15:09:39 +0300

    initial commit

---
+++ cache/smarty/templates_c/%%C6^C60^C6011F11%%_headerSearch.tpl.php
@@ -0,0 +1,2 @@
+<?php /* Smarty version 2.6.11, created on 2017-04-24 13:21:52
+         compiled from themes/Suite7/tpls/_headerSearch.tpl */ ?>
---
+++ custom/modules/Meetings/Ext/Vardefs/vardefs.ext.php
@@ -0,0 +1,36 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+
+$dictionary['Meeting']['fields']['SecurityGroups'] = array (
+  	'name' => 'SecurityGroups',
+    'type' => 'link',
+	'relationship' => 'securitygroups_meetings',
+	'module'=>'SecurityGroups',
+	'bean_name'=>'SecurityGroup',
+    'source'=>'non-db',
+	'vname'=>'LBL_SECURITYGROUPS',
+);
+
+
+
+
+
+
+ // created: 2017-04-24 13:12:46
+
+ 
+
+ // created: 2017-04-24 13:12:46
+
+ 
+
+ // created: 2017-04-24 13:12:46
+
+ 
+
+ // created: 2017-04-24 13:12:46
+
+ 
+?>
